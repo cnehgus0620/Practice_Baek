b0c75b836054c3537ad5026e32f85af82be9d7ef
cnehgus0620 <cnehgus0620@naver.com>
2022-09-29 05:35:14 +0900

    Commit

--- Practice_Baek/2083_럭비클럽.c
+++ Practice_Baek/2083_럭비클럽.c
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main() {
+int club() {
 	int age, weight;
 	char name[10];
 	while (1) {
---
+++ Practice_Baek/2439_별 찍기.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+	int main() {
+	int n;
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (j < n - i - 1) printf(" ");
+			else printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
